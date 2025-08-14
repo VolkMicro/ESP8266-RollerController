3a7105700b6231bdd1743fcab4164445223f24a1
VolkMicro <153424262+VolkMicro@users.noreply.github.com>
2025-08-14 13:26:51 +0300

    Document environment-based secrets

--- src/Secrets.cpp
+++ src/Secrets.cpp
@@ -1,6 +1,19 @@
 #include "Secrets.h"
 
-const char* WIFI_SSID = "your_wifi_ssid";
-const char* WIFI_PASSWORD = "your_wifi_password";
-const char* MQTT_HOST = "your.mqtt.host";
-const char* OTA_PASSWORD = "your_ota_password";
+#ifndef WIFI_SSID_VAL
+#error "WIFI_SSID environment variable not defined"
+#endif
+#ifndef WIFI_PASSWORD_VAL
+#error "WIFI_PASSWORD environment variable not defined"
+#endif
+#ifndef MQTT_HOST_VAL
+#error "MQTT_HOST environment variable not defined"
+#endif
+#ifndef OTA_PASSWORD_VAL
+#error "OTA_PASSWORD environment variable not defined"
+#endif
+
+const char* WIFI_SSID = WIFI_SSID_VAL;
+const char* WIFI_PASSWORD = WIFI_PASSWORD_VAL;
+const char* MQTT_HOST = MQTT_HOST_VAL;
+const char* OTA_PASSWORD = OTA_PASSWORD_VAL;
